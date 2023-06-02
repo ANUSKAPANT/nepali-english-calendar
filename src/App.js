aa95254effe8a61e96c5c35a6fd4feef65a5270d
anuskapant <anuskapant@gmail.com>
2023-06-02 04:19:51 -0500

    initial design added

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import logo from './logo.svg';
+import './App.css';
+import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css';
+import NepaliDate from 'nepali-date-converter';
+
+const datesToAddClassTo = [{"09-02-2080": "Dad's bday"}, {"29-02-2080": "Asmi Bday"}, {"02-03-2080": "Mom's bday"}, {"28-05-2080": "Father's day"}];
+
+function tileClassName({ date, view }) {
+  // Add class to tiles in month view only
+  if (view === 'month') {
+    // Check if a date React-Calendar wants to check is on the list of dates to add class to
+    if (datesToAddClassTo.find(dDate => (new NepaliDate(date) - new NepaliDate(Object.keys(dDate)[0])) === 0)) {
+      return 'react-calendar__tile--event';
+    }
+  }
+}
+
+
+function tileContent({date, view}) {
+  const nepaliDate = new NepaliDate(date).format('DD MMMM')
+  if (view === 'month') {
+    var key = '';
+    var val = '';
+    // Check if a date React-Calendar wants to check is on the list of dates to add class to
+    if (datesToAddClassTo.find(dDate => {
+      key = Object.keys(dDate)[0];
+      val = Object.values(dDate)[0];
+      const isEvent = (new NepaliDate(date) - new NepaliDate(key)) === 0;
+      return isEvent;
+    })) {
+      return (<><div className="nepali-date">{nepaliDate}</div><div>{val}</div></>)
+    } else {
+      return (<div className="nepali-date">{nepaliDate}</div>)
+    }
+  }
+
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Calendar
+        tileContent={tileContent} 
+        tileClassName={tileClassName}
+      />
+    </div>
+  );
+}
+
+export default App;
